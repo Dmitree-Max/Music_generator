f51e0e4126da9e372421a57cb8339086264b3e6e
Dmitree-Max <d.s.maximenko@gmail.com>
2019-01-31 15:59:24 +0300

    commit 01/01/2019

---
+++ simple_generator_doc.py
@@ -0,0 +1,12 @@
+
+
+"""It is description of our module"""
+
+def make_music_rand():
+    """This func generates random sequence"""
+    pass
+
+
+def write_sequence(list):
+    """This function writes list of arguments into a mid file"""
+    pass 
